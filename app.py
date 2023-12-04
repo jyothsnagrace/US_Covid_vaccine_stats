06f354fb7c1eb03a8547ebe9f1cb6b508f9caff4
jyothsnagrace <leelajosna.kona@gmail.com>
2023-12-04 18:42:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ import certifi, os
 certifi.where()
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
-# def load_data():
-#     url = 'https://data.cdc.gov/resource/n8mc-b4w4.csv?$query=SELECT%0A%20%20%60res_state%60%2C%0A%20%20%60case_month%60%2C%0A%20%20%60age_group%60%2C%0A%20%20%60sex%60%2C%0A%20%20%60ethnicity%60%2C%0A%20%20%60race%60%2C%0A%20%20count(*)%20AS%20%60count%60%0AGROUP%20BY%0A%20%20%60res_state%60%2C%0A%20%20%60case_month%60%2C%0A%20%20%60age_group%60%2C%0A%20%20%60sex%60%2C%0A%20%20%60race%60%2C%0A%20%20%60ethnicity%60'
-#     df = pd.read_csv(url)
-#     # data = df.rename(columns={'long': 'lon'})
-#     # data['month_sold'] = pd.to_datetime(data['date']).dt.month
-#     return df
+def load_data():
+    url = 'https://data.cdc.gov/resource/n8mc-b4w4.csv?$query=SELECT%0A%20%20%60res_state%60%2C%0A%20%20%60case_month%60%2C%0A%20%20%60age_group%60%2C%0A%20%20%60sex%60%2C%0A%20%20%60ethnicity%60%2C%0A%20%20%60race%60%2C%0A%20%20count(*)%20AS%20%60count%60%0AGROUP%20BY%0A%20%20%60res_state%60%2C%0A%20%20%60case_month%60%2C%0A%20%20%60age_group%60%2C%0A%20%20%60sex%60%2C%0A%20%20%60race%60%2C%0A%20%20%60ethnicity%60'
+    df = pd.read_csv(url)
+    # data = df.rename(columns={'long': 'lon'})
+    # data['month_sold'] = pd.to_datetime(data['date']).dt.month
+    return df
 
 
-# df = load_data()
+df = load_data()
 
-# df
-# df.shape
+df
+df.shape
 
 
 # df=[]
@@ -39,13 +39,13 @@ os.environ['SSL_CERT_FILE'] = certifi.where()
 
 
 
-import pandas as pd
-import io
-import requests
-url = 'https://data.cdc.gov/resource/n8mc-b4w4.csv?$query=SELECT%0A%20%20%60res_state%60%2C%0A%20%20%60case_month%60%2C%0A%20%20%60age_group%60%2C%0A%20%20%60sex%60%2C%0A%20%20%60ethnicity%60%2C%0A%20%20%60race%60%2C%0A%20%20count(*)%20AS%20%60count%60%0AGROUP%20BY%0A%20%20%60res_state%60%2C%0A%20%20%60case_month%60%2C%0A%20%20%60age_group%60%2C%0A%20%20%60sex%60%2C%0A%20%20%60race%60%2C%0A%20%20%60ethnicity%60'
-s=requests.get(url).content
-c=pd.read_csv(io.StringIO(s.decode('utf-8')))
+# import pandas as pd
+# import io
+# import requests
+# url = 'https://data.cdc.gov/resource/n8mc-b4w4.csv?$query=SELECT%0A%20%20%60res_state%60%2C%0A%20%20%60case_month%60%2C%0A%20%20%60age_group%60%2C%0A%20%20%60sex%60%2C%0A%20%20%60ethnicity%60%2C%0A%20%20%60race%60%2C%0A%20%20count(*)%20AS%20%60count%60%0AGROUP%20BY%0A%20%20%60res_state%60%2C%0A%20%20%60case_month%60%2C%0A%20%20%60age_group%60%2C%0A%20%20%60sex%60%2C%0A%20%20%60race%60%2C%0A%20%20%60ethnicity%60'
+# s=requests.get(url).content
+# c=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
 
-c
-c.shape
+# c
+# c.shape
