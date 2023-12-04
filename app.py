5647fa175cca3700658b49b8e3ae8648304574ca
Leela Josna Kona <leelajosna.kona@gmail.com>
2023-12-04 18:58:24 -0500

    commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def load_data():
-    df = pd.read_csv("/Users/leelajosnakona/Leela/GitHub/US_Covid_vaccine_stats/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography.csv")
+    df = pd.read_csv("COVID-19_Case_Surveillance_Public_Use_Data_with_Geography.csv")
     return df
 
 
