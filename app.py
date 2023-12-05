cafa75346574c6e0531d4f8e6f03d420776021fa
Leela Josna Kona <leelajosna.kona@gmail.com>
2023-12-04 19:00:58 -0500

    c

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ def load_data():
     return df
 
 
+st.set_page_config(page_title="Covid 19 Vaccinated Demographics", page_icon=":mask", layout="wide")
+
+
+st.title(":mask: _United States:_ Covid 19 Vaccinated Demographics :syringe: :mask:")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+
+
 df = load_data()
 
 df
